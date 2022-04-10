b1f6bcec8e6a2e37d41d5a3046c7512cb06c8eb9
johannesedwinw <johannesedwin.wuisan@georgebrown.ca>
2022-04-10 01:35:05 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate, Router } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { ListComponent as BookingListComponent } from './booking/list/list.component';
+import { NewComponent as BookingNewComponent } from './booking/new/new.component';
+import { ListComponent as ListingListComponent} from './listing/list/list.component';
+import { NewComponent as ListingNewComponent } from './listing/new/new.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "listing",
+    component: ListingListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "new-listing",
+    component: ListingNewComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "booking",
+    component: BookingListComponent,
+    canActivate: [AuthGuard]
+  },{
+    path: "new-booking/:listing_id",
+    component: BookingNewComponent,
+    canActivate: [AuthGuard]
+  },{ path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
